fcf563b00fc513a57e37e0b994e673b70873e488
Jose Antonio Hernandez Hernandez <l23120514@morelia.tecnm.mx>
2025-02-18 09:36:37 -0600

    Clase 3 y 4

---
+++ segmentacion del color.py
@@ -0,0 +1,22 @@
+import cv2 as cv   #importa openCv
+import numpy as np  # Importa Numpy
+
+img = cv.imread('manzanas.jpeg',1)
+hsv=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+
+uba=(10,255,255)
+ubb=(0,100,100)
+
+uba2=(180,255,255)
+ubb2=(172,100,100)
+mask1=cv.inRange(hsv,ubb,uba)
+mask2=cv.inRange(hsv,ubb2,uba2)
+mask=mask1+mask2
+res=cv.bitwise_and(img,img,mask=mask)
+cv.imshow('mask1',mask1)
+cv.imshow('mask2',mask2)
+cv.imshow('res',res)
+
+
+cv.waitKey(0)
+cv.destroyAllWindows
